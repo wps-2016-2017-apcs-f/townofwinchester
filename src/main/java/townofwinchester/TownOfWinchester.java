43f89a90386d99dac9ba1eb911f74d936f8644f1
wps-dpetty <dpetty@winchesterps.org>
2017-05-27 00:51:52 -0400

    Rearranged and renamed files to go into the townofwinchester package.

---
+++ src/main/java/townofwinchester/TownOfWinchester.java
@@ -0,0 +1,12 @@
+package townofwinchester;
+
+/**
+ * TownOfWinchester
+ */
+public class TownOfWinchester
+{
+    public static void main( String[] args )
+    {
+        System.out.println("# TownOfWinchester");
+    }
+}
--- src/test/java/townofwinchester/TownOfWinchesterTest.java
+++ src/test/java/townofwinchester/TownOfWinchesterTest.java
@@ -1,4 +1,4 @@
-package org.winchesterps;
+package townofwinchester;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,15 +7,14 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class TownOfWinchesterTest extends TestCase
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public TownOfWinchesterTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +24,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( TownOfWinchesterTest.class );
     }
 
     /**
