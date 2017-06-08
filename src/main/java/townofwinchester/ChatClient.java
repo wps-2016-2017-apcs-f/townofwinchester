5d678899cb3b049210c816622241d0d27aedc93f
Roy Xing <00royxing@gmail.com>
2017-06-08 13:45:07 -0400

    added the logger messages for ChatClient and ChatServer and fixed some deprecated functions

--- src/main/java/townofwinchester/ChatClient.java
+++ src/main/java/townofwinchester/ChatClient.java
@@ -4,55 +4,66 @@
 package townofwinchester;
 
 import java.net.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.*;
 
+//console.readline() and stop() thread is depracted
 /**
  * ChatClient is the main class for the client part of the chat
  *
  * @see http://pirate.shu.edu/~wachsmut/Teaching/CSAS2214/Virtual/Lectures/chat-client-server.html
+ *
+ * @author Roy H. Xing
  */
 
 public class ChatClient implements Runnable
 {  private Socket socket              = null;
    private Thread thread              = null;
-   private DataInputStream  console   = null;
+   private BufferedReader   console   = null;
    private DataOutputStream streamOut = null;
    private ChatClientThread client    = null;
+   private static Logger logger = LogManager.getLogger("ChatClient");
+   private volatile boolean runningThread = true;
 
-   public ChatClient(String serverName, int serverPort)
-   {  System.out.println("Establishing connection. Please wait ...");
+   public ChatClient(String serverName, int serverPort){
+	  logger.info("Establishing connection. Please wait ...");
       try
       {  socket = new Socket(serverName, serverPort);
-         System.out.println("Connected: " + socket);
+         logger.info("Connected: " + socket);
          start();
       }
-      catch(UnknownHostException uhe)
-      {  System.out.println("Host unknown: " + uhe.getMessage()); }
-      catch(IOException ioe)
-      {  System.out.println("Unexpected exception: " + ioe.getMessage()); }
+      catch(UnknownHostException uhe){
+		  logger.info("Host unknown: " + uhe.getMessage());
+	  }
+      catch(IOException ioe){
+		  logger.info("Unexpected exception: " + ioe.getMessage());
+	  }
    }
    public void run()
-   {  while (thread != null)
+   {  while (thread != null && runningThread)
       {  try
          {  streamOut.writeUTF(console.readLine());
             streamOut.flush();
          }
          catch(IOException ioe)
-         {  System.out.println("Sending error: " + ioe.getMessage());
+         {
+			logger.info("Sending error: " + ioe.getMessage());
             stop();
          }
       }
    }
    public void handle(String msg)
    {  if (msg.equals(".bye"))
-      {  System.out.println("Good bye. Press RETURN to exit ...");
+      {  
+		 logger.info("Good bye. Press RETURN to exit...");
          stop();
       }
       else
          System.out.println(msg);
    }
    public void start() throws IOException
-   {  console   = new DataInputStream(System.in);
+   {  console   = new BufferedReader(new InputStreamReader(System.in));
       streamOut = new DataOutputStream(socket.getOutputStream());
       if (thread == null)
       {  client = new ChatClientThread(this, socket);
@@ -60,25 +71,25 @@ public class ChatClient implements Runnable
          thread.start();
       }
    }
-   public void stop()
-   {  if (thread != null)
-      {  thread.stop();  
-         thread = null;
-      }
-      try
-      {  if (console   != null)  console.close();
+   public void stop(){
+	   if(thread != null){
+		   runningThread = false;
+		   //thread = null;
+		}
+       try{
+		 if (console   != null)  console.close();
          if (streamOut != null)  streamOut.close();
          if (socket    != null)  socket.close();
       }
-      catch(IOException ioe)
-      {  System.out.println("Error closing ..."); }
-      client.close();  
-      client.stop();
+      catch(IOException ioe){
+		  logger.info("Error, closing..."); }
+		  client.close();  
+          client.stop();
    }
    public static void main(String args[])
    {  ChatClient client = null;
       if (args.length != 2)
-         System.out.println("Usage: java ChatClient host port");
+		 logger.info("You did not execute the program correctly, do this: Usage: java ChatClient host port");
       else
          client = new ChatClient(args[0], Integer.parseInt(args[1]));
    }
--- src/main/java/townofwinchester/ChatServer.java
+++ src/main/java/townofwinchester/ChatServer.java
@@ -4,12 +4,16 @@
 package townofwinchester;
 
 import java.net.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.io.*;
 
 /**
  * ChatServer is the main class for the server part of the chat
  *
  * @see http://pirate.shu.edu/~wachsmut/Teaching/CSAS2214/Virtual/Lectures/chat-client-server.html
+ * javadoc comments by:
+ * @author Roy H. Xing
  */
 
 public class ChatServer implements Runnable
@@ -17,44 +21,85 @@ public class ChatServer implements Runnable
    private ChatServerThread clients[] = new ChatServerThread[50];
    private ServerSocket server = null;
    private Thread       thread = null;
+   private static Logger logger = LogManager.getLogger("ChatServer");
    private int clientCount = 0;
 
    public ChatServer(int port)
    {  try
-      {  System.out.println("Binding to port " + port + ", please wait  ...");
+      {  
+		 logger.info("Binding to port " + port + ", please wait  ...");
          server = new ServerSocket(port);  
-         System.out.println("Server started: " + server);
+		 logger.info("Server started: " + server);
          start(); }
       catch(IOException ioe)
-      {  System.out.println("Can not bind to port " + port + ": " + ioe.getMessage()); }
+      {   
+		 logger.info("Can not bind to port " + port + ": " + ioe.getMessage());
+	  }
    }
+   
+   /**
+    * The run method that runs the actual client and adds
+	* the initialized client to the thread
+	* @return Nothing
+	* @exception IOException on server accepting error
+    */
    public void run()
    {  while (thread != null)
       {  try
-         {  System.out.println("Waiting for a client ..."); 
+         {  
+			logger.info("Waiting for a client...");
             addThread(server.accept()); }
          catch(IOException ioe)
-         {  System.out.println("Server accept error: " + ioe); stop(); }
+         {  
+			logger.info("Server accept error: " + ioe);
+			stop(); 
+		 }
       }
    }
+   
+   /**
+    * The start method that starts the thread for the
+	* clients
+	* @return Nothing.
+    */
    public void start()
    {  if (thread == null)
       {  thread = new Thread(this); 
          thread.start();
       }
    }
+   
+   /**
+    * The stop method that terminates the threads
+	* of the clients
+	* @return Nothing.
+    */
    public void stop()
    {  if (thread != null)
       {  thread.stop(); 
          thread = null;
       }
    }
+   
+   /**
+    * The method to find the client's ID
+	*
+	* @param ID This is the passed identification number of the client
+	* @return int This returns the client's ID
+    */
    private int findClient(int ID)
    {  for (int i = 0; i < clientCount; i++)
          if (clients[i].getID() == ID)
             return i;
       return -1;
    }
+   
+   /**
+    * The method to handle the messages sent
+	* @param input This is the string message to be handled
+	* @param ID This is the passed identification number of the client
+	* @return Nothing.
+    */
    public synchronized void handle(int ID, String input)
    {  if (input.equals(".bye"))
       {  clients[findClient(ID)].send(".bye");
@@ -63,11 +108,17 @@ public class ChatServer implements Runnable
          for (int i = 0; i < clientCount; i++)
             clients[i].send(ID + ": " + input);   
    }
+   
+   /**
+    * This is the method to remove a client from the chat
+	* @param ID This is the passed identification number of the client
+	* @return Nothing.
+    */   
    public synchronized void remove(int ID)
    {  int pos = findClient(ID);
       if (pos >= 0)
       {  ChatServerThread toTerminate = clients[pos];
-         System.out.println("Removing client thread " + ID + " at " + pos);
+		 logger.info("Removing client thread " + ID + " at " + pos);
          if (pos < clientCount-1)
             for (int i = pos+1; i < clientCount; i++)
                clients[i-1] = clients[i];
@@ -75,26 +126,40 @@ public class ChatServer implements Runnable
          try
          {  toTerminate.close(); }
          catch(IOException ioe)
-         {  System.out.println("Error closing thread: " + ioe); }
+         {  logger.info("Error closing thread: " + ioe); }
          toTerminate.stop(); }
    }
+   
+   /**
+    * This method adds a client thread to the socket for the chat
+	* @param socket This is the socket to add to the thread
+	* @return Nothing.
+    */
    private void addThread(Socket socket)
    {  if (clientCount < clients.length)
-      {  System.out.println("Client accepted: " + socket);
+      {  
+		 logger.info("Client accepted: " + socket);
          clients[clientCount] = new ChatServerThread(this, socket);
          try
          {  clients[clientCount].open(); 
             clients[clientCount].start();  
             clientCount++; }
          catch(IOException ioe)
-         {  System.out.println("Error opening thread: " + ioe); } }
+         {  logger.info("Error opening thread: " + ioe); } }
       else
-         System.out.println("Client refused: maximum " + clients.length + " reached.");
+		 logger.info("Client refused: maximum " + clients.length + " reached.");
    }
+   
+   /**
+    * This is the main method
+	* @param args This is the argument that allows for the port #
+	* example of usage: java ClientServer 50501
+	* @return Nothing
+    */
    public static void main(String args[])
    {  ChatServer server = null;
       if (args.length != 1)
-         System.out.println("Usage: java ChatServer port");
+		 logger.info("Usage: java ChatServer port");
       else
          server = new ChatServer(Integer.parseInt(args[0]));
    }
