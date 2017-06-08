612334e4d3d46d3bf179d72f49e39dcd8f9d631b
Ethan Wong <ewong2200@gmail.com>
2017-06-08 13:45:55 -0400

    src/test/java/townofwinchester/MessagesTest.java

---
+++ src/test/java/townofwinchester/MessagesTest.java
@@ -0,0 +1,19 @@
+/*
+ * MessagesTest.java
+ */
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * MesssagesTest is the test class for Messages.java
+ *
+ *
+ * @author Jeremy Hudges
+ * @author Ethan Wong
+ */
+
+public class MessagesTest
+{  
+	
+}
