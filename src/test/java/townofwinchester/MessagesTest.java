19c4dc8b27a2cbaf03d41398cba760fdaca81d4a
Ethan Wong <ewong2200@gmail.com>
2017-06-13 12:02:15 -0400

    Edited the MessagesTest Class to follow up with the Messages Class

--- src/test/java/townofwinchester/MessagesTest.java
+++ src/test/java/townofwinchester/MessagesTest.java
@@ -13,9 +13,30 @@ import junit.framework.TestSuite;
  * @author Ethan Wong
  */
 
-public class MessagesTest
-{  
-	
+public class MessagesTest extends TestCase{
+	/**
+     * This creates the test case
+     *
+     * @param testName name of the test case
+     */
+	public Messages( String testName )
+    	{
+     	   super( testName );
+   		}
+   	/**
+     * @return the suite of tests being tested
+     */
+   	public static Test suite()
+    	{
+        	return new TestSuite( CharacterTest.class );
+    	}	
 
+    /**
+     * Rigorous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 
 }
