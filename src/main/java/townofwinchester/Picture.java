5e1b2b7f682af796787b4a2d3ce67a2f5d20d96d
Roy Xing <00royxing@gmail.com>
2017-06-20 13:50:35 -0400

    added the Picture class for drawing and overriding the painComponent method, also for Andy: https://stackoverflow.com/questions/12335121/initializing-a-graphics-variable

---
+++ src/main/java/townofwinchester/Picture.java
@@ -0,0 +1,27 @@
+/*
+ * ChatServer.java
+ */
+package townofwinchester;
+
+import java.net.*;
+import java.io.*;
+import org.apache.logging.log4j.*;
+import java.util.Scanner;
+import java.awt.*;
+
+/**
+ * This class is for drawing on the GUI
+ *
+ * @author Andy Ark
+ * @author Roy Xing
+ */
+
+public class Picture extends JPanel{
+	private int a;
+	public Picture(){
+		a = 0;
+	}
+	public void paintComponent(Graphics g){
+		
+	}
+}
