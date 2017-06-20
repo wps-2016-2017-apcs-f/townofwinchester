ce91c68ab1ae293d55bbf8fb807fbf75a615026d
wps-dpetty <dpetty@winchesterps.org>
2017-06-20 13:56:45 -0400

    Changed Message.java to have two constructors: one for parsing a String message and one for constructing a message from parts.

--- src/main/java/townofwinchester/Message.java
+++ src/main/java/townofwinchester/Message.java
@@ -2,10 +2,10 @@
  * Message.java
  */
 package townofwinchester;
-//import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.*;
 import java.net.*;
 import java.io.*;
- 
+
 /**
  * Messsage is the wrapper class for the messages that will be sent throughout the game
  *
@@ -15,62 +15,75 @@ import java.io.*;
  *
  */
 public class Message{  
-    public String nextCommand;
     public static enum Type {CHAT, MCHAT, JOIN, PICK, TIME, MVOTE, VOTE};
-    public String text;
-    public String name;
-    //public Character god,sender,receiver;
- 
-    /** 
-      * Describes the Message that is being Snet
-      * 
-      * @from args is used to indicate the character that is sending the message. 
-      * 
-      * @to args is used to indicate the character that will eventually receive the message and is able to access it. 
-      *
-      * @details describes the content of the message
-     */
-      //public Message(Character from, Character to, String details) {
-          // sender = from;
-          // receiver = to; 
-          // message = details; 
-      //}
+    private Type type;
+    private String name;
+    private String text;
 
-      public void parseMessage(String message) 
-      {
-          for (Type a: Type.values()) {
-              switch (a) {
-              case CHAT:
-                  text = message.substring(message.lastIndexOf(":") + 1, message.length());
-                  break;
-              case MCHAT:
-                  text = message.substring(message.lastIndexOf(":") + 1, message.length());
-                  break;
-              case JOIN:
-                                   
-              case PICK:
-              case TIME:
-              case MVOTE:
-              case VOTE:
-              break;
-              }
-              name = message.substring(message.indexOf(":") + 1, message.lastIndexOf(":"));
-         }
-      }
- public String assignRole(boolean indicateMafiaStatus, String name) {
-  //Character character = new Character();
-  //String name1 = character.getName();
-  if (indicateMafiaStatus == true){ 
-   nextCommand = name + "." + "toMafia()";
-   return Type.PICK + "You are a Mafia";
-    } 
-  else{   
-   nextCommand = name + "." + "toVillager()"; 
-   return Type.PICK + name + "is a villager";
- } 
- }
- 
- public String compileText(String name, String message) {
-   return Type.CHAT + ":" + name + ":" + message; 
- }
+    /** 
+     * Describes the Message that is being Snet
+     * 
+     * @from args is used to indicate the character that is sending the message. 
+     * 
+     * @to args is used to indicate the character that will eventually receive the message and is able to access it. 
+     *
+     * @details describes the content of the message
+     */
+    //public Message(Character from, Character to, String details) {
+    // sender = from;
+    // receiver = to; 
+    // message = details; 
+    //}
+    
+    // Example: "CHAT:NAME:Hi, there!"
+    public Message(String message) {
+        String[] parts = message.split(":");
+        assert parts.length >= 3 : message + " must have (at least) two ':'";
+        for (Type type : Type.values())
+            if (type.toString().equals(parts[0].toUpperCase())) {
+                this.type = type;
+                this.name = parts[1];
+                this.text = parts[2];
+                for (int i = 3; i < parts.length; i++)
+                    this.text += ":" + parts[i];
+            }
+        assert this.type != null && this.name != null && this.text != null
+        : "String message parse failed";
+        LogManager.getLogger(TownOfWinchester.SHORT).debug("Message({})", toString());
+    }
+
+    public Message(Type type, String name, String text) {
+        this.type = type;
+        this.name = name;
+        this.text = text;
+    }
+
+    public Type getType() { return type; }
+    public String getName() { return name; }
+    public String getText() { return text; }
+
+    public String toString() {
+        return type.toString() + ":" + name + ":" + text;
+    }
+
+    // RED_FLAG: obsolete methods
+
+    private String nextCommand;
+
+    public String assignRole(boolean indicateMafiaStatus, String name) {
+        //Character character = new Character();
+        //String name1 = character.getName();
+        if (indicateMafiaStatus == true){ 
+            nextCommand = name + "." + "toMafia()";
+            return Type.PICK + "You are a Mafia";
+        } 
+        else{   
+            nextCommand = name + "." + "toVillager()"; 
+            return Type.PICK + name + "is a villager";
+        } 
+    }
+
+    public String compileText(String name, String message) {
+        return Type.CHAT + ":" + name + ":" + message; 
+    }
 }
--- src/test/java/townofwinchester/MessageTest.java
+++ src/test/java/townofwinchester/MessageTest.java
@@ -7,22 +7,20 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
 /**
  * MesssageTest is the test class for Message.java
- *
  *
  * @author Jeremy Hughes
  * @author Ethan Wong
  */
 
-public class MessageTest extends Message{
- public static void main (String[] args) {
-  Message test = new Message();
-  System.out.println(test.assignRole(false, "James "));
-  System.out.println(test.compileText("James", "We should kill David"));
-  test.parseMessage("CHAT:Tim:Oh no David died");
-  System.out.println(test.text);
-  System.out.println(test.name);
- }
+public class MessageTest extends TestCase {
+    public static void main (String[] args) {
+        Message test = new Message("CHAT:Tim:Oh no David died");
+        System.out.println(test.assignRole(false, "James "));
+        System.out.println(test.compileText("James", "We should kill David"));
+        System.out.println(test.getType());
+        System.out.println(test.getText());
+        System.out.println(test.getName());
+    }
 }
